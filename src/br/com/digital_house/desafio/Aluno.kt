c27119353635bbe179a8b8ed78ee114366667b81
mariliamessias <marilia.messias@mercadolivre.com>
2020-09-25 21:47:09 -0300

    Create Aluno class

---
+++ src/br/com/digital_house/desafio/Aluno.kt
@@ -0,0 +1,17 @@
+package br.com.digital_house.desafio
+
+class Aluno (var nome : String, var sobrenome : String , var codigo : Int ) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Aluno
+        if (codigo != other.codigo) return false
+        return true
+    }
+
+    override fun toString(): String {
+        return "Aluno(nome='$nome', sobrenome='$sobrenome', codigo=$codigo)"
+    }
+}
