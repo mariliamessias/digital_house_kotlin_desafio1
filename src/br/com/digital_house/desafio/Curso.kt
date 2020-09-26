6c6df91a669b7f61a8afc6f2dd2959bd89ff8b6e
mariliamessias <marilia.messias@mercadolivre.com>
2020-09-25 22:02:24 -0300

    create class Curso

--- src/br/com/digital_house/desafio/Curso.kt
+++ src/br/com/digital_house/desafio/Curso.kt
@@ -1,18 +1,18 @@
 package br.com.digital_house.desafio
 
-class Aluno (var nome : String, var sobrenome : String , var codigo : Int ) {
+class Curso (var nome : String, var codigo: Int){
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as Aluno
+        other as Curso
         if (codigo != other.codigo) return false
 
         return true
     }
 
     override fun toString(): String {
-        return "Aluno(nome='$nome', sobrenome='$sobrenome', codigo=$codigo)"
+        return "Curso(nome='$nome', codigo=$codigo)"
     }
 }
