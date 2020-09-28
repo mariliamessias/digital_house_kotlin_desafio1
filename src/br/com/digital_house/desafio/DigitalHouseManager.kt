85934e2c112934fd4320cba3872d1f72ce2b6092
mariliamessias <marilia.messias@mercadolivre.com>
2020-09-28 19:47:45 -0300

    create class DigitalHouseManager

---
+++ src/br/com/digital_house/desafio/DigitalHouseManager.kt
@@ -0,0 +1,4 @@
+package br.com.digital_house.desafio
+
+class DigitalHouseManager(alunos: MutableList<Aluno>, matriculas : MutableList<Matricula>, cursos : MutableList<Curso> ,professores : MutableList<Professor>) {
+}
