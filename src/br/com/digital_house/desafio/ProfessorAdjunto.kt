bfd6ce1f38aa77d7bd4a19a1bb666309d6063db7
mariliamessias <marilia.messias@mercadolivre.com>
2020-09-27 23:58:30 -0300

    create ProfessorAdjunto and ProfessorTitular class

---
+++ src/br/com/digital_house/desafio/ProfessorAdjunto.kt
@@ -0,0 +1,4 @@
+package br.com.digital_house.desafio
+
+class ProfessorAdjunto (var quantidadeHorasMonitoria : Int, nome : String, sobrenome: String, tempoDeCasa: Int, codigo: Int): Professor(nome, sobrenome, tempoDeCasa, codigo ) {
+}
